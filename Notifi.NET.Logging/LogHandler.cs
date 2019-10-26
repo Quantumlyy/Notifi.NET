aae0564565b9e1511274ca026df326beafe7cfb8
Nejc <ethoarx@gmail.com>
2019-10-26 21:30:10 +0200

    Logging

---
+++ Notifi.NET.Logging/LogHandler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using Notifi.NET.Logging.Enums;
+
+using Console = Colorful.Console;
+
+namespace Notifi.NET.Logging
+{
+    public class LogHandler<T>
+    {
+        private static readonly Lazy<LogHandler<T>> LazyLogHandler
+            = new Lazy<LogHandler<T>>(() => new LogHandler<T>());
+
+        public static LogHandler<T> Log
+            => LazyLogHandler.Value;
+
+        private readonly object _logLock;
+        private readonly DateTimeOffset _date;
+
+        public LogHandler()
+        {
+            _logLock = new object();
+            _date = DateTimeOffset.Now;
+        }
+
+        public void Information(string message, Exception exception = default)
+        {
+            RawLog(LogLevel.Information, message, exception);
+        }
+
+        public void UsageUpdates(string message, Exception exception = default)
+        {
+            RawLog(LogLevel.UsageUpdates, message, exception);
+        }
+
+        public void RawLog(LogLevel logLevel, string message, Exception exception, string logPoster = "?")
+        {
+            lock (_logLock)
+            {
+                var date = $"[{DateTimeOffset.Now:MMM d - hh:mm:ss tt}]";
+                var log = $" [{GetLogLevel(logLevel)}] ";
+                var msg = exception?.ToString() ?? message;
+                var loggedName = logPoster != "?" ? logPoster : typeof(T).Name;
+                var logMessage = $"{date}{log}[{loggedName}] {msg}";
+
+                Append(date, Color.Gray);
+                Append(log, GetColor(logLevel));
+                Append(msg, Color.White);
+                Console.Write(Environment.NewLine);
+
+                WriteToFile(logMessage);
+            }
+        }
+
+        private static void Append(string message, Color color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+        }
+
+        private static string GetLogLevel(LogLevel logLevel)
+        {
+            return logLevel switch
+            {
+                LogLevel.Critical => "CRIT",
+                LogLevel.Debug => "DBUG",
+                LogLevel.Error => "EROR",
+                LogLevel.Information => "INFO",
+                LogLevel.None => "NONE",
+                LogLevel.Trace => "TRCE",
+                LogLevel.Warning => "WARN",
+                LogLevel.UsageUpdates => "USGU",
+                _ => "NONE"
+            };
+        }
+
+        private static Color GetColor(LogLevel logLevel)
+        {
+            return logLevel switch
+            {
+                LogLevel.Critical => Color.Red,
+                LogLevel.Debug => Color.SlateBlue,
+                LogLevel.Error => Color.Red,
+                LogLevel.Information => Color.SpringGreen,
+                LogLevel.None => Color.BurlyWood,
+                LogLevel.Trace => Color.SlateBlue,
+                LogLevel.Warning => Color.Yellow,
+                LogLevel.UsageUpdates => Color.LightSeaGreen,
+                _ => Color.SlateBlue
+            };
+        }
+
+        private void WriteToFile(string message)
+        {
+            File.AppendAllText($"{_date.Year}-{_date.Month}-{_date.Day}.log", $"{message}\n");
+        }
+    }
+}
--- Notifi.NET.Singletons/Singletons.cs
+++ Notifi.NET.Singletons/Singletons.cs
@@ -3,12 +3,13 @@ using System.Net.Http;
 
 namespace Notifi.NET.Singletons
 {
-    public static class Singletons
+    public class Singletons
     {
-        private static readonly Lazy<HttpClient> LazyHttpClient = new Lazy<HttpClient>(() => new HttpClient());
+        private static readonly Lazy<HttpClient> LazyHttpClient
+            = new Lazy<HttpClient>(() => new HttpClient());
 
-        private static readonly Lazy<Random> LazyRandom =
-            new Lazy<Random>();
+        private static readonly Lazy<Random> LazyRandom
+            = new Lazy<Random>();
 
         public static HttpClient HttpClient
             => LazyHttpClient.Value;
